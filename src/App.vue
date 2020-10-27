ab2b820f7fdb4df370336440047b60112d22ad4f
isenTang <3104255643@qq.com>
2020-10-27 15:41:03 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,49 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <router-link to="/">
+        Home
+      </router-link> |
+      <router-link to="/about">
+        About
+      </router-link>
+    </div>
+    <router-view />
+
+    <Loading />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+/* components */
+import Loading from './components/Loading/Loading';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+   components:{
+      Loading
+   }
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family : Avenir, Helvetica, Arial, sans-serif;
+  color : #2c3e50;
+  text-align : center;
+  -webkit-font-smoothing : antialiased;
+  -moz-osx-font-smoothing : grayscale;
+}
+
+#nav {
+  padding : 30px;
+}
+
+#nav a {
+  font-weight : bold;
+  color : #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color : #42b983;
 }
 </style>
